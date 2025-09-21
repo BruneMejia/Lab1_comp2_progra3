6dcfbaa828d5098cf4195f9d01987498ad7c3150
BruneMejia <madeline.mej07@gmail.com>
2025-09-20 21:30:12 -0600

    Cambios

--- ControlVentan.py
+++ ControlVentan.py
@@ -1,1 +1,10 @@
-print("hi")
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QLineEdit, QMessageBox)
+from PyQt5.QtGui import QIcon
+import sys
+
+app = QApplication(sys.argv)
+
+ventana = QWidget()
+ventana.setWindowTitle("CONTROL DE VENTAS DE VERDURAS")
+
+ventana.setGeometry(500,500,500,500)
