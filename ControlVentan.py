decfc5cae2af0d13ec3035a04191f907d411ab89
BruneMejia <madeline.mej07@gmail.com>
2025-09-21 18:09:27 -0600

    Actualizacion final

--- ControlVentan.py
+++ ControlVentan.py
@@ -4,33 +4,30 @@ from PyQt5.QtWidgets import (QApplication, QWidget, QVBoxLayout, QPushButton, QL
 from PyQt5.QtGui import QIcon
 import sys
 #Aquí se importan las librerías necesarias para crear la interfaz gráfica de usuario (GUI) utilizando PyQt5.
-#En esta parte se crea la aplicación 
 #Creacion de la aplicacion
 
 app = QApplication(sys.argv)
-#En esta parte se define la función para agregar una venta a la lista
-#Funcion para agragar la venta 
+#función para agregar una venta a la lista
 def venta():
     producto = entrada1.text()
     cantidad = entrada2.text()
     monto = entrada3.text()
-#En esta parte se valida que los campos no esten vacios ya que si lo estan se muestra un mensaje de error
+# validacion, que los campos no esten vacios ya que si lo estan se muestra un mensaje de error
     if producto and cantidad and monto:
         try:
 #Se valida que el monto sea un número válido, si no lo es se muestra un mensaje de error
-#En esta parte se convierte el monto a float para que se muestre con dos decimales
+#se convierte el monto a float para que se muestre con dos decimales
             monto_float = float(monto)
             lista_ventas.addItem(f" #{cantidad} {producto}  ${monto_float:.2f}")
             entrada1.clear()
             entrada2.clear()
             entrada3.clear()
         except ValueError:
-#En esta parte se muestra un mensaje de error si el monto no es un número válido
+#muestra un mensaje de error si el monto no es un número válido
             QMessageBox.warning(ventana, "Error", "El monto debe ser un nmero valido.")
     else:
-        QMessageBox.warning(ventana, "Error", "Por favor ingrese rl producto y monto.")
-#En esta parte se define la función para eliminar una venta de la lista
-#Funcion para eliminar la venta
+        QMessageBox.warning(ventana, "Error", "Por favor ingrese el productom, la cantidad y el monto.")
+#función para eliminar una venta de la lista
 #En esta parte se valida que se haya seleccionado un item de la lista para eliminarlo, si no se muestra un mensaje de error
 #Si esta seleccionado se elimina el item de la lista
 def eliminar():
@@ -39,8 +36,7 @@ def eliminar():
         lista_ventas.takeItem(lista_ventas.row(item))
     else:
         QMessageBox.warning(ventana, "Error", "Porfa seleccione una venta para eliminar.")
-#En esta parte se crea la ventana principal de la aplicación
-#Creacion de la ventana
+#Creacion de la ventana principal de la aplicación
 #Se define el título, tamaño e icono de la ventana
 #Luego se crean los elementos de la interfaz (etiquetas, campos de entrada, botones y lista)
 #Finalmente se muestra la ventana y se inicia el bucle de eventos de la aplicación
